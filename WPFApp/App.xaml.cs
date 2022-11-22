84e5454db6a2c3a82e6db90810d292f4f8a0eed2
dhq_boiler <dhq_boiler@live.jp>
2022-11-22 15:40:20 +0900

    add WPF calculator project

---
+++ WPFApp/App.xaml.cs
@@ -0,0 +1,29 @@
+﻿using Prism.Ioc;
+using Prism.Unity;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using WPFApp.Views;
+
+namespace WPFApp
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : PrismApplication
+    {
+        protected override Window CreateShell()
+        {
+            var w = Container.Resolve<MainWindow>();
+            return w;
+        }
+
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+        }
+    }
+}
