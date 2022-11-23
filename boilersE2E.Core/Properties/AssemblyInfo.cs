7403c77ae0288f8112ed5c7fdadd3dac5ce5f2ca
dhq_boiler <dhq_boiler@live.jp>
2022-11-23 11:23:01 +0900

    replace folder boilersE2E.Core

--- boilersE2E.Core/Properties/AssemblyInfo.cs
+++ boilersE2E.Core/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // アセンブリに関する一般情報は以下の属性セットをとおして制御されます。
 // 制御されます。アセンブリに関連付けられている情報を変更するには、
 // これらの属性値を変更してください。
-[assembly: AssemblyTitle("boilerE2E")]
+[assembly: AssemblyTitle("boilerE2E.Core")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("boilerE2E")]
+[assembly: AssemblyProduct("boilerE2E.Core")]
 [assembly: AssemblyCopyright("Copyright ©dhq_boiler 2022")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
